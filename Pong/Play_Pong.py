1148c4ec6c75690e60bb8dafa68293b400d4770c
Diyalo Manral <dmanral@asu.edu>
2019-11-08 13:49:03 -0700

    Win message added.
    Cleaned code, added a winner message, and the game resets once a winner is established.

--- Pong/Play_Pong.py
+++ Pong/Play_Pong.py
@@ -3,84 +3,14 @@
 
 import turtle
 import winsound
+import ctypes
+import os
 from Pong_Build_Game import *
 from Pong_Game_Play import *
-
-# wn = turtle.Screen()
-# wn.title("Pong")
-# wn.bgcolor("black")
-# wn.setup(width=800, height=600)
-# wn.tracer(0)
 
 # Score
 score_1 = 0
 score_2 = 0
-
-# # Paddle A
-# paddle_a = turtle.Turtle()
-# paddle_a.speed(0)               # Speed of animation
-# paddle_a.shape("square")
-# paddle_a.color("white")
-# paddle_a.shapesize(stretch_wid=5, stretch_len=1)
-# paddle_a.penup()
-# paddle_a.goto(-350, 0)
-
-# # Paddle B
-# paddle_b = turtle.Turtle()
-# paddle_b.speed(0)               # Speed of animation
-# paddle_b.shape("square")
-# paddle_b.color("white")
-# paddle_b.shapesize(stretch_wid=5, stretch_len=1)
-# paddle_b.penup()
-# paddle_b.goto(350, 0)
-
-# # Ball
-# ball = turtle.Turtle()
-# ball.speed(0)               # Speed of animation
-# ball.shape("square")
-# ball.color("white")
-# ball.penup()
-# ball.goto(0, 0)
-# ball.dx = 0.1 # Delta
-# ball.dy = -0.1
-
-# # Pen
-# pen = turtle.Turtle()
-# pen.speed(0)
-# pen.color("white")
-# pen.penup()
-# pen.hideturtle()
-# pen.goto(0, 260)
-# pen.write("Player-1: 0  Player-2: 0", align="center", font=("Courier", 24, "normal"))
-
-# # Function
-# def paddle_a_up():
-#     y = paddle_a.ycor()
-#     y += 20
-#     paddle_a.sety(y)
-
-# def paddle_a_down():
-#     y = paddle_a.ycor()
-#     y -= 20
-#     paddle_a.sety(y)
-
-# def paddle_b_up():
-#     y = paddle_b.ycor()
-#     y += 20
-#     paddle_b.sety(y)
-
-# def paddle_b_down():
-#     y = paddle_b.ycor()
-#     y -= 20
-#     paddle_b.sety(y)
-
-
-# # Keyboard binding
-# wn.listen()
-# wn.onkeypress(paddle_a_up, "w")
-# wn.onkeypress(paddle_a_down, "s")
-# wn.onkeypress(paddle_b_up, "Up")
-# wn.onkeypress(paddle_b_down, "Down")
 
 # Main game loop
 while True:
@@ -94,12 +24,14 @@ while True:
     if ball.ycor() > 290:
         ball.sety(290)
         ball.dy *= -1
-        winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
+        os.system("afplay bounce.wav&")                      # MacOS
+        winsound.PlaySound("bounce.wav", winsound.SND_ASYNC) # Windows
     
     if ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1
-        winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
+        os.system("afplay bounce.wav&")                      # MacOS
+        winsound.PlaySound("bounce.wav", winsound.SND_ASYNC) # Windows
 
     if ball.xcor() > 390:
         ball.goto(0, 0)
@@ -119,9 +51,25 @@ while True:
     if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor () < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
         ball.setx(340)
         ball.dx *= -1
-        winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
+        os.system("afplay bounce.wav&")                      # MacOS
+        winsound.PlaySound("bounce.wav", winsound.SND_ASYNC) # Windows
     
     if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor () < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40):
         ball.setx(-340)
         ball.dx *= -1
-        winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
+        os.system("afplay bounce.wav&")                      # MacOS
+        winsound.PlaySound("bounce.wav", winsound.SND_ASYNC) # Windows
+
+    # Winner
+    if score_1 == 7 and score_2 < 7:
+        ctypes.windll.user32.MessageBoxW(0, "Player-1 wins the game with a score of {} to {}".format(score_1, score_2), "Game Over!", 0)
+        score_1 = 0
+        score_2 = 0
+        pen.clear()
+        pen.write("Player-1: {}  Player-2: {}".format(score_1, score_2), align="center", font=("Courier", 24, "normal"))
+    elif score_2 == 7 and score_1 < 7:
+        ctypes.windll.user32.MessageBoxW(0, "Player-2 wins the game with a score of {} to {}".format(score_2, score_1), "Game Over!", 0)
+        score_1 = 0
+        score_2 = 0
+        pen.clear()
+        pen.write("Player-1: {}  Player-2: {}".format(score_1, score_2), align="center", font=("Courier", 24, "normal"))
